f1e99d5d26555f9c6b700d81b79ce8ad649738ab
nameismrudula <148964208+nameismrudula@users.noreply.github.com>
2024-03-10 20:50:07 +0000

    Update 4-CI-build-pipeline.groovy

--- 4-CI-build-pipeline.groovy
+++ 4-CI-build-pipeline.groovy
@@ -1,7 +1,7 @@
 pipeline {   
   agent any
   environment {     
-    DOCKERHUB_CREDENTIALS= credentials('dockerhubcredentials')     
+    DOCKERHUB_CREDENTIALS= credentials('docker')     
   }    
   stages {         
     stage("Git Checkout"){           
